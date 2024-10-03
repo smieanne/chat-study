77b4f139df6795c91deb35be8a33431b427a5b8a
Mieanne Sugiyama <mieanne.sugiyama@digitalwallet.co.jp>
2024-10-03 11:08:54 +0900

    修正

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://api.openai.com/v1/chat/completions',
+  openaiApiKey: '', // 空のままにしておく
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://api.openai.com/v1/chat/completions',
+  openaiApiKey: '', // 空のままにしておく
+};
